21d0d1d1fec6c6d8f687631cff0fed1bbf283a69
TehLily <tehlily@gmail.com>
2023-01-14 16:08:55 -0600

    Initial commit

---
+++ Topic02/main.py
@@ -0,0 +1,26 @@
+"""
+
+Program: main.py
+
+Author: Lily Ellison
+
+Last date modified: 01/12/2023
+
+The purpose of this program is convert age in years to months.
+
+The input is an integer representing the age in years.
+
+The output an integer representing the age in months.
+
+"""
+
+#This is a comment
+print('Hello World')
+
+#Name variable
+users_name = 'Joe'
+
+#print out Hello to the user's name
+print('Hello' + ' ' + users_name + '. How are you?')
+#print out the type of the name
+print("The 'users_name' variable is type: " + str(type(users_name)))
